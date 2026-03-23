ba29085e9893c22cb4a4d0512fdf9364f585ca29
Lilyennd <victoriariffo1@gmail.com>
2026-03-23 12:24:46 -0300

    avances

---
+++ src/main/java/cl/biblioteca/BibliotecaDuoc/BibliotecaDuocApplication.java
@@ -0,0 +1,13 @@
+package cl.biblioteca.BibliotecaDuoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaDuocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaDuocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/biblioteca/BibliotecaDuoc/BibliotecaDuocApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.biblioteca.BibliotecaDuoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaDuocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
